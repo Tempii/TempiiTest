90c0e4fb4624d276c02c057cd211f98644103ca1
Kai_Rechner <kai.von.holdt@uni-oldenburg.de>
2015-05-19 12:57:57 +0200

    Angriffsmuster

--- GitTest/src/model/Enemy.java
+++ GitTest/src/model/Enemy.java
@@ -3,14 +3,17 @@ package model;
 public abstract class Enemy {
 
 	int speed;
-	
-	public Enemy(int speed){
+
+	public Enemy(int speed) {
 		this.speed = speed;
 	}
 
-	
-	public void attack(){
-		
+	public void attack1() {
+
 	}
-	
+
+	public void attack2() {
+
+	}
+
 }
